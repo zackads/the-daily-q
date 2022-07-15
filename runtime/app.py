02ba684e192824355d0f5d4c8ff358fccbe79b02
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-07-15 19:55:15 +0100

    Extract email body to function

--- runtime/app.py
+++ runtime/app.py
@@ -4,50 +4,37 @@ import random
 from chalice import Chalice
 from botocore.exceptions import ClientError
 
-SENDER = "The Daily Q <zadlington@gmail.com>"
-RECIPIENT = "zadlington@gmail.com"
-AWS_REGION = "eu-west-2"
+SENDER = "The Daily Q <" + {os.environ.get("SENDER")} + ">"
+RECIPIENT = os.environ.get("RECIPIENT")
 SUBJECT = "The Daily Q"
-
-BODY_TEXT = "This is a test email"
-BODY_HTML = """<html>
-<head></head>
-<body>
-    <h1>The Daily Q</h1>
-    <p>This is a test email
-</body>
-</html>
-"""
-CHARSET = "UTF-8"
-
 
 app = Chalice(app_name="the-daily-q")
 s3 = boto3.resource("s3")
 s3_bucket = s3.Bucket(os.environ.get("S3_BUCKET_NAME", ""))
-ses = boto3.client("ses", region_name=AWS_REGION)
-
+ses = boto3.client("ses", region_name="eu-west-2")
 
 @app.route("/questions", methods=["GET"])
 def get_questions():
-    questions = [
-        "https://the-daily-q.s3.eu-west-2.amazonaws.com/" + q.key
-        for q in s3_bucket.objects.all()
-    ]
+    S3_URL = "https://the-daily-q.s3.eu-west-2.amazonaws.com/"
+    questions = [q.key for q in s3_bucket.objects.filter(Prefix='questions/')].sort()
+    solutions = [s.key for s in s3_bucket.objects.filter(Prefix='solutions/')].sort()
 
-    return questions
+    return list(zip(questions, solutions))
 
 
 @app.route("/email", methods=["POST"])
 def send_email():
+    question, solution = random_question()
+
     try:
         response = ses.send_email(
             Destination={"ToAddresses": [RECIPIENT]},
             Message={
                 "Body": {
-                    "Html": {"Charset": CHARSET, "Data": BODY_HTML,},
-                    "Text": {"Charset": CHARSET, "Data": BODY_TEXT},
+                    "Html": {"Charset": "UTF-8", "Data": body_html(question, solution)},
+                    "Text": {"Charset": "UTF-8", "Data": body_plaintext(question, solution)},
                 },
-                "Subject": {"Charset": CHARSET, "Data": SUBJECT},
+                "Subject": {"Charset": "UTF-8", "Data": SUBJECT},
             },
             Source=SENDER,
         )
@@ -59,13 +46,28 @@ def send_email():
 
 def random_question():
     """
-    Select a question from the question bank and return its URL
+    Select a question from the question bank.
+    
+    Return a tuple of the question URL and the solution URL
     """
-    questions = [
-        "https://the-daily-q.s3.eu-west-2.amazonaws.com/" + q.key
-        for q in s3_bucket.objects.all()
-    ]
+    questions = get_questions()
     i = random.randrange(0, questions.length)
 
     return questions[i]
 
+def body_plaintext(question_url, solution_url):
+    return ("The Daily Q"
+    f"Question: {question_url}"
+    f"Solution: {solution_url}"
+    )
+
+def body_html(question_url, solution_url):
+    return """<html>
+<head></head>
+<body>
+    <h1>The Daily Q</h1>
+    <img src="{}" />
+    <a href="{}">Solution</a>
+</body>
+</html>
+""".format(question_url, solution_url)
